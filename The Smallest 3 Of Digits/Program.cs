6f9d4c29b9d45f5d577839837f27e2fc1fc1403f
SamirAliyevv <samirsa@code.edu.az>
2023-01-16 14:56:40 -0800

    master

---
+++ The Smallest 3 Of Digits/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace The_Smallest_3_Of_Digits
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            int a = 3;
+            int b = 2;
+            int c = 6;
+
+            if (a <= b && a <= c)
+            {
+                Console.WriteLine("A ededi en kicik ededdir");
+            }
+
+
+            else if (b <= c && b <= a)
+            {
+
+                Console.WriteLine(" B ededi en kicik ededdir");
+            }
+            else if (c <= a && c <= b)
+            {
+                Console.WriteLine("C  ededi en kicik ededdir");
+
+
+
+
+
+
+
+
+
+
+            }
+
+
+        }
+    }
+    
+}
